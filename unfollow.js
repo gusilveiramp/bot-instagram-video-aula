be611f32f64b62f451d0b3ad40d4868d6b1e2a13
Gustavo Silveira <gusilveiramp@gmail.com>
2020-04-24 05:19:26 -0300

    add follow unfollow

--- unfollow.js
+++ unfollow.js
@@ -1,17 +1,3 @@
-// Seguir automaticamente
-let contador = 0;
-document.querySelectorAll('.L3NKy').forEach((item, index) => {
-    setTimeout(() => {
-		if(!item.classList.contains('_8A5w5')){
-        	item.click();
-        	contador++;
-			console.log('Seu BOT já seguiu ' + contador + ' pessoa(s)!');
-		} else { 
-			console.log('Você já segue essa pessoa.');
-        };
-    }, index * 20000);
-});
-
 // Deixar de seguir automaticamente
 let contador = 0;
 document.querySelectorAll('._8A5w5').forEach((item, index) => {
