a7195dce340f25a5133bfb7d2d38ff6f38348a81
Gustavo Silveira <gusilveiramp@gmail.com>
2020-04-28 17:05:50 -0300

    like

---
+++ like.js
@@ -0,0 +1,24 @@
+// Curtir automaticamente
+const next = document.querySelector('a.coreSpriteRightPaginationArrow');
+var counter = 0;
+function doLike() {
+  const like_btn = document.querySelector('article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button');
+  if(like){
+      console.log('click\n');
+      like_btn.click();
+      counter++;
+  }
+  next.click();
+  console.log(`Você já curtiu ${counter} post(s)!`);
+}
+
+(function loop() {
+    setTimeout(() => {
+      doLike();
+      loop();  
+    }, 10000); // 10 segundos
+}());
+
+
+
+
