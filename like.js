b239f903faf6e72aab2a45cf3f73c7068fd0f131
Gustavo Silveira <gusilveiramp@gmail.com>
2020-04-29 18:12:55 -0300

    like

--- like.js
+++ like.js
@@ -1,9 +1,10 @@
 // Curtir automaticamente
 const next = document.querySelector('a.coreSpriteRightPaginationArrow');
 var counter = 0;
+
 function doLike() {
   const like_btn = document.querySelector('article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button');
-  if(like){
+  if(like_btn){
       console.log('click\n');
       like_btn.click();
       counter++;
@@ -18,7 +19,5 @@ function doLike() {
       loop();  
     }, 10000); // 10 segundos
 }());
-
-
 
 
