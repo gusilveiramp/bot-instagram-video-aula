444ad2650484c351a1c980eb90edbed1df9e051d
Gustavo Silveira <gusilveiramp@gmail.com>
2020-04-30 15:03:10 -0300

    follow

--- follow.js
+++ follow.js
@@ -1,13 +1,15 @@
 // Seguir automaticamente
 let contador = 0;
-document.querySelectorAll('.L3NKy').forEach((item, index) => {
-    setTimeout(() => {
+let arr = document.querySelectorAll('.L3NKy');
+
+arr.forEach((item, index) => {
+  setTimeout(() => {
 		if(!item.classList.contains('_8A5w5')){
         	item.click();
         	contador++;
-			console.log('Seu BOT já seguiu ' + contador + ' pessoa(s)!');
+			console.log(`Seu BOT já seguiu ${contador} pessoa(s)!`);
 		} else { 
 			console.log('Você já segue essa pessoa.');
-        };
-    }, index * 20000);
+    };
+  }, index * 20000);
 });
